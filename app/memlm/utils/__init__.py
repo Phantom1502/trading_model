4c18ac52604fdc9d24f1f2b91f997d16a0c39885
sullivan1502 <nguyen.huy.701@gmail.com>
2026-06-22 21:58:45 +0700

    U

--- app/memlm/utils/__init__.py
+++ app/memlm/utils/__init__.py
@@ -1,4 +1,4 @@
 from .checkpoint import save_checkpoint, load_checkpoint
-from .logger import TrainLogger, log_eval
+from .logger import TrainLogger, log_eval, log_bench
 
-__all__ = ["save_checkpoint", "load_checkpoint", "TrainLogger", "log_eval"]
+__all__ = ["save_checkpoint", "load_checkpoint", "TrainLogger", "log_eval", "log_bench"]
